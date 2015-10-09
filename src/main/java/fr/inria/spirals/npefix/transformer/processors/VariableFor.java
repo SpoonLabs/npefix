1e69fa9af13a0ea524f1f1caed13427219e64567
tdurieux <durieuxthomas@hotmail.com>
2015-10-09 15:52:32 +0200

    Add a processor to check variable in for and foreach

---
+++ src/main/java/fr/inria/spirals/npefix/transformer/processors/VariableFor.java
@@ -0,0 +1,46 @@
+package fr.inria.spirals.npefix.transformer.processors;
+
+import fr.inria.spirals.npefix.resi.CallChecker;
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.*;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.reference.CtExecutableReference;
+import spoon.support.reflect.code.CtInvocationImpl;
+
+import java.util.Arrays;
+
+/**
+ * Verify that a variable is not null in a loop or a foreach loop if(varA !=null) for(a in varA)
+ */
+public class VariableFor extends AbstractProcessor<CtVariableRead<?>> {
+
+	@Override
+	public void process(CtVariableRead<?> element) {
+		CtElement parent = element.getParent();
+		if(!(parent instanceof CtForEach
+				|| parent instanceof CtFor))
+			return;
+		// for variable declared in a for and used oin the declaration ex: for(boolean loop = true;loop;)
+		if(element.getVariable().getDeclaration().getParent().equals(element.getParent())) {
+			return;
+		}
+		CtExecutableReference execif = getFactory().Core().createExecutableReference();
+		execif.setDeclaringType(getFactory().Type().createReference(CallChecker.class));
+		execif.setSimpleName("beforeDeref");
+		execif.setStatic(true);
+
+		CtInvocationImpl ifInvoc = (CtInvocationImpl) getFactory().Core().createInvocation();
+		ifInvoc.setExecutable(execif);
+		ifInvoc.setArguments(Arrays.asList(new CtExpression[]{element}));
+
+		CtIf encaps = getFactory().Core().createIf();
+		encaps.setCondition(ifInvoc);
+
+		CtBlock thenBloc = getFactory().Core().createBlock();
+
+		((CtStatement) parent).replace(encaps);
+		encaps.setThenStatement(thenBloc);
+		thenBloc.addStatement((CtStatement) parent);
+	}
+
+}
