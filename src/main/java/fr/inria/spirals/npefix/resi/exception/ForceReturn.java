81ff0231a1d3e624892b9ecf5ad57b7dacd8f62f
danglotb <bdanglot@gmail.com>
2017-07-12 15:37:34 +0200

    replace wildcard imports by single imports

--- src/main/java/fr/inria/spirals/npefix/resi/exception/ForceReturn.java
+++ src/main/java/fr/inria/spirals/npefix/resi/exception/ForceReturn.java
@@ -3,7 +3,6 @@ package fr.inria.spirals.npefix.resi.exception;
 import fr.inria.spirals.npefix.resi.context.Decision;
 
 public class ForceReturn extends RuntimeException {
-
 	private Decision decision;
 
 	public ForceReturn(Decision decision) {
