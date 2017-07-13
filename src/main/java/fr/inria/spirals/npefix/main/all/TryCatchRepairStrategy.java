dab0458d980dae45ded8d586f053a8c1ce46233e
danglotb <bdanglot@gmail.com>
2017-07-13 13:38:45 +0200

    refactor method run in TryCatchRepairStrategy

--- src/main/java/fr/inria/spirals/npefix/main/all/TryCatchRepairStrategy.java
+++ src/main/java/fr/inria/spirals/npefix/main/all/TryCatchRepairStrategy.java
@@ -1,22 +1,15 @@
 
 package fr.inria.spirals.npefix.main.all;
 
-import fr.inria.spirals.npefix.resi.CallChecker;
 import fr.inria.spirals.npefix.resi.context.Decision;
-import fr.inria.spirals.npefix.resi.context.Lapse;
 import fr.inria.spirals.npefix.resi.context.Location;
 import fr.inria.spirals.npefix.resi.context.NPEOutput;
-import fr.inria.spirals.npefix.resi.oracle.TestOracle;
 import fr.inria.spirals.npefix.resi.selector.Selector;
 import fr.inria.spirals.npefix.resi.strategies.Strategy;
 import fr.inria.spirals.npefix.transformer.processors.*;
-import org.junit.runner.Request;
-import org.junit.runner.Result;
-import utils.sacha.runner.main.TestRunner;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -49,42 +42,9 @@ public class TryCatchRepairStrategy extends DefaultRepairStrategy {
 		processors.add(new VariableFor());//
 	}
 
-	@Override
 	public NPEOutput run(Selector selector, List<String> methodTests) {
 		TryCatchRepairStrategy.selector = selector;
-		NPEOutput output = new NPEOutput();
-		Lapse lapse = new Lapse(selector);
-		final TestRunner testRunner = new TestRunner();
-		for (int i = 0; i < methodTests.size(); i++) {
-			String method = methodTests.get(i);
-			String[] split = method.split("#");
-			method = split[1];
-			String className = split[0];
-			final Request request;
-			try {
-				request = Request.method(CallChecker.currentClassLoader.loadClass(className), method);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				continue;
-			}
-			lapse.setTestClassName(className);
-			lapse.setTestName(method);
-			try {
-				selector.startLaps(lapse);
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
-			Result result = testRunner.run(request);
-
-			lapse.setOracle(new TestOracle(result));
-			System.out.println(lapse);
-			if (result.getRunCount() > 0) {
-				output.add(lapse);
-			}
-			lapse = new Lapse(selector);
-		}
-		Collections.sort(output);
-		return output;
+		return super.run(selector, methodTests);
 	}
 
 	public static Decision<Object> getDecision(Class clazz, String className, int line, int sourceStart, int sourceEnd) {
